efcae64a4a2182bf5659165ab9c23401330cc7dc
Benjiisaac <isaacbenjamin.bamba@gmail.com>
2022-12-10 00:14:22 +0200

    bonus 1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,27 +6,26 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	int i, j;
-	
-	for (i = 0; i <= 9; i++)
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
-			if (i != j)
-			{
-				if (i == 8 && j == 9)
-					continue;
-				printf("%d", i);
-				putchar(j + '0');
-				putchar(',');
-				putchar(' ');
-			}
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+			
+			putchar(',');
+			putchar(' ');
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
-
