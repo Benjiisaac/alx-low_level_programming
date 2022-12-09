d14242eaf2879c7783b963c2646ce8e8442cb8a9
Benjiisaac <isaacbenjamin.bamba@gmail.com>
2022-12-09 23:51:04 +0200

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - Prints all single digit numbers of base 10 starting from 0.
+ * main - Prints all possible combinations of single-digit numbers.
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	int i, j;
+	int num;
 
-	for (i = 0; i <= 9; i++)
+	for (num = 0; num <= 9; num++)
 	{
-		for (j = 0; j <= 9; j++)
-		{
-			printf("%d", i);
-			printf(", ");
-			printf("%d", j);
-			printf(", ");
-		}
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
 	}
+
 	putchar('\n');
+
 	return (0);
 }
