32b69118072b9a52d1bc38151088bd586cb4cb1e
Benjiisaac <isaacbenjamin.bamba@gmail.com>
2022-12-13 21:05:35 +0200

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int _lower - a function that returns 1 if lowercase 0 otherwise
+ *
+ * Return: 
+ */
+
+int _islower(int c)
+{
+	int c;
+
+	if ('a' <= c && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
