daa4dc18a0d88477d6e4a72a292abf1eee855556
Benjiisaac <isaacbenjamin.bamba@gmail.com>
2022-12-13 20:45:01 +0200

    task 1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+ *
+ * Return: x10 a-z
+ */
+
+void print_alphabet_x10(void)
+{
+	int c;
+	int counter = 0;
+
+	while (counter < 10)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar(a);
+		}
+		counter++;
+	}
+	_putchar('\n');
+}
