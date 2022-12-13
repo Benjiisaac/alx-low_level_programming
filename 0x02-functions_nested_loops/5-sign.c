90cdc04058456d4ff835be29aba77bed6ac5cfff
Benjiisaac <isaacbenjamin.bamba@gmail.com>
2022-12-13 21:59:49 +0200

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_sign - check if a character is alphabetical
+ * @c: the character to check
+ *
+ * Return: 1 if c is alpha, 0 otherwise
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		return ('+');
+	else if (n == 0)
+		return (0);
+	else
+		return ('-');
+}
