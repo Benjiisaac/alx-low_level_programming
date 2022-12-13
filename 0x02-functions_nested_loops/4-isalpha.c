789b4adaf8a58676ed8d5ff97da38ad64beb3aa3
Benjiisaac <isaacbenjamin.bamba@gmail.com>
2022-12-13 21:36:50 +0200

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,7 +9,9 @@
 
 int _islower(int c)
 {
-	if (('a' <= c && c <= 'z') || ('A' <= c && c <= 'z'))
+	if ('a' <= c && c <= 'z')
+		return (1);
+	else if ('A' <= c && c<= 'Z')
 		return (1);
 	else
 		return (0);
