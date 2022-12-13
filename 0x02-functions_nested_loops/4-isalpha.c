c1876dad9e5777e993c9be7b3f47e8c94b83d198
Benjiisaac <isaacbenjamin.bamba@gmail.com>
2022-12-13 21:22:16 +0200

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - check if a character is alphabetical
+ * @c: the character to check
+ *
+ * Return: 1 if c is alpha, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if ('a' <= c && c <= 'z' | 'A' <= c && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
