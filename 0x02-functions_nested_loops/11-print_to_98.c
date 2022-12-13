137fa23b94ea5cf2c1612826d07ce6cd60ce4b9e
Benjiisaac <isaacbenjamin.bamba@gmail.com>
2022-12-14 00:30:29 +0200

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,27 +9,33 @@
 
 void print_to_98(int n)
 {
-	
-	if (n <= 98)
+	if (n < 98)
 	{
 		while (n <= 98)
 		{
-			_putchar(n + '0');
-			_putchar(',');
-			_putchar(' ');
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
 			n++;
 		}
 	}
-	if (n >= 98)
+	else if (n > 98)
 	{
 		while (n >= 98)
 		{
-			_putchar(n + '0');
-			_putchar(',');
-			_putchar(' ');
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
 			n--;
 		}
 	}
-	if (n == 98)
-		putchar(n + '0');
+	else
+	{
+		printf("98");
+	}
+	printf("\n");
 }
