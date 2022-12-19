11997d0e6ba4a6868dd97f106a9d310f812c4d3a
Benjiisaac <isaacbenjamin.bamba@gmail.com>
2022-12-20 00:33:55 +0200

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,10 +14,12 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] + 1 != '\0'; i += 2)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
-
+		if (a % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
