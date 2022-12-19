eb32f1b6325a3c774fbd875b83b12edd9bac463b
Benjiisaac <isaacbenjamin.bamba@gmail.com>
2022-12-20 00:52:05 +0200

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - Write a function that prints every other character of a string,
+ *         starting with the first character, followed by a new line.
+ *
+ * @str: This pointer point to a char or string
+ *
+ *
+ */
+
+void puts_half(char *str)
+{
+	int L = strlen(str) / 2;
+
+	for (i = L / 2 + L % 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
