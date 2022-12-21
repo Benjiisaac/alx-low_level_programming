f0d7cb840c20ef6504558009c24a4de00ab53e26
Benjiisaac <isaacbenjamin.bamba@gmail.com>
2022-12-22 00:43:17 +0200

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,28 +10,18 @@
 char *leet(char *s)
 {
 	int i;
+	int j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; ++i)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
+		for (j = 0; j < 5; j++)
 		{
-			s[i] = '4';
-		}
-		else if (s[i] == 'e' || s[i] == 'E')
-		{
-			s[i] = '3';
-		}
-		else if (s[i] == 'o' || s[i] == 'O')
-		{
-			s[i] = '0';
-		}
-		else if (s[i] == 't' || s[i] == 'T')
-		{
-			s[i] = '7';
-		}
-		else if (s[i] == 'l' || s[i] == 'L')
-		{
-			s[i] = '1';
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
 		}
 	}
 	return (s);
