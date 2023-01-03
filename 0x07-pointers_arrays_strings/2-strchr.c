f5e352f1475799b92d3cd7376df96475e4517b73
Benjiisaac <isaacbenjamin.bamba@gmail.com>
2023-01-03 22:39:12 +0200

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,9 +1,9 @@
 #include <stddef.h>
+#include "main.h"
 /**
  * _strchr - locates a character in a string
  * @s: a char pointer to the string to search
  * @c: the character to search for
- *
  * Return: pointer to s[i] or NULL
  */
 char *_strchr(char *s, char c)
