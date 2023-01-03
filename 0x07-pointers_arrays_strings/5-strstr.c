257e53a54b88ce39d37b86bfb04fefaefe44a20b
Benjiisaac <isaacbenjamin.bamba@gmail.com>
2023-01-04 00:14:29 +0200

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,19 +14,16 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		if (haystack[i] == needle[0])
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			for (j = 0; needle[j] != '\0'; j++)
+			if (haystack[i + j] != needle[j])
 			{
-				if (haystack[i + j] != needle[j] || haystack[i + j] == '\0')
-				{
-					break;
-				}
+				break;
 			}
-			if (needle[j] == '\0')
-			{
-				return (&haystack[i]);
-			}
+		}
+		if (needle[j] == '\0')
+		{
+			return (haystack + i);
 		}
 	}
 	return (NULL);
