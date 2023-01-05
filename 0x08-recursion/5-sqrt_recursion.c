17abfc9b6b23a8c696bb6e9b99f036f8384be350
Benjiisaac <isaacbenjamin.bamba@gmail.com>
2023-01-06 00:53:39 +0200

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,11 +3,10 @@
  * sqrt2 - Makes possible to evaluate from 1 to n
  * @a: same number as n
  * @b: number that iterates from 1 to n
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  * _sqrt_recursion - returns the natural square root of n
- * @n: Number Integer.
- */
+ * @n: Number Integer
+ * Return: On success 1, -1 on error
+ **/
 int sqrt2(int a, int b)
 {
 	if (b * b == a)
