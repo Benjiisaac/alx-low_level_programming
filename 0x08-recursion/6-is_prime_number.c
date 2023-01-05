6e196f7dd90e482d96f416e583c267a29a70ab6b
Benjiisaac <isaacbenjamin.bamba@gmail.com>
2023-01-06 01:12:49 +0200

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,19 +6,26 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n < 2)
+	if (n <= 1)
 		return (0);
-	if (n == 2)
+	return (prime2(n, 2));
+}
+
+/**
+ * prime2 - Makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int prime2(int a, int b)
+{
+	if (a == b)
 		return (1);
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-			return (0);
-	}
-	return (1);
+	else if (a % b == 0)
+		return (0);
+	return (prime2(a, b + 1));
 }
