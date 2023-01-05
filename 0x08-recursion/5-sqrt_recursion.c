98023179f949ed8179bec4b95a1f58405ae24ab9
Benjiisaac <isaacbenjamin.bamba@gmail.com>
2023-01-06 00:34:39 +0200

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,11 +10,13 @@
  */
 int _sqrt_recursion(int n)
 {
+	int root;
+
   if (n == 0 || n == 1)
   {
     return n;
   }
-  int root = _sqrt_recursion(n - 1);
+  root = _sqrt_recursion(n - 1);
 
   if (root * root == n)
   {
