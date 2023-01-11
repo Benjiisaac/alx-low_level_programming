8e38c0cee470ebbf93918f08048b5871fdd3395e
Benjiisaac <isaacbenjamin.bamba@gmail.com>
2023-01-12 00:36:20 +0200

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,13 +13,13 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *array;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	*array = (char *) malloc(sizeof(char) * size);
+
+	char *array = (char *) malloc(sizeof(char) * size);
 	if (array == NULL)
 	{
 		return (NULL);
