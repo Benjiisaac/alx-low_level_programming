0d81a8c1f074cfaa3f23b90c37749765303d5784
Benjiisaac <isaacbenjamin.bamba@gmail.com>
2023-01-17 23:22:02 +0200

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct - structure of type dog
+ * @name: pointer to name of dog
+ *
+ * @age: age of dog
+ * @owner: pointer to the owners name
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
